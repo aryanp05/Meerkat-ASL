f4189748742bffc60d9faf8a3853fcc4539fecbb
Aryan Patel <aryanpatel@Aryans-Air.phub.net.cable.rogers.com>
2024-05-18 14:27:27 -0400

    GUI Created

---
+++ Meerkat_GUI.py
@@ -0,0 +1,148 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(1)
+
+ret, frame = cap.read()
+H, W, _ = frame.shape
+
+def point_inside_rect(x, y, rect):
+    x1, y1, w, h = rect
+    return x1 <= x <= x1 + w and y1 <= y <= y1 + h
+
+def mouse_callback(event, x, y, flags, param):
+    global box1_hover, box2_hover
+    global gradient_frame, image
+    global instruction_show
+    global H, W
+
+    if point_inside_rect(x, y, (box_x1, box_y, box_width, box_height)):
+        box1_hover = True
+        if event == cv2.EVENT_LBUTTONDOWN:
+            from Meerkat_Main_F import ASL_mode
+    else:
+        box1_hover = False
+
+    if point_inside_rect(x, y, (box_x2, box_y, box_width, box_height)):
+        box2_hover = True
+        if event == cv2.EVENT_LBUTTONDOWN:
+            instruction_show = True
+
+    else:
+        box2_hover = False
+
+
+gradient_frame = np.zeros((H, W, 3), dtype=np.uint8)
+colour_1 = (210, 170, 109)
+colour_2 = (255, 215, 0)
+
+
+for y in range(H):
+    for x in range(W):
+        factor = (x/W * y/H)
+
+        red = abs(int(colour_1[0] - abs(int(colour_1[0] - colour_2[0]) * factor)))
+        green = abs(int(colour_1[1] - abs(int(colour_1[1] - colour_2[1]) * factor)))
+        blue = abs(int(colour_1[2] - abs(int(colour_1[2] - colour_2[2]) * factor)))
+
+ 
+        gradient_frame[y, x] = (blue, green, red)
+
+font_size = 5
+text_size = cv2.getTextSize("Meerkat ASL", cv2.FONT_HERSHEY_TRIPLEX, font_size, 2)[0]
+text_x = (W - text_size[0]) // 2
+text_y = (H - text_size[1]) // 2
+cv2.putText(gradient_frame, "Meerkat ASL", (text_x, text_y), cv2.FONT_HERSHEY_TRIPLEX, font_size, (255,255,255), 2)
+
+box_width = 500
+box_height = 200
+box_space = 50
+box_x1 = (W - 2 * box_width - box_space) // 2
+box_x2 = box_x1 + box_width + box_space
+box_y = text_y + text_size[1] + 50
+cv2.rectangle(gradient_frame, (box_x1, box_y), (box_x1 + box_width, box_y + box_height),(255,255,255), -1)
+text1 = "Start"
+text_size1 = cv2.getTextSize(text1, cv2.FONT_HERSHEY_TRIPLEX, 2, 2)[0]
+text_x1 = box_x1 + (box_width - text_size1[0]) // 2
+text_y1 = box_y + (box_height + text_size1[1]) // 2
+cv2.putText(gradient_frame, text1, (text_x1, text_y1), cv2.FONT_HERSHEY_TRIPLEX, 2, (0, 0, 0), 2)
+cv2.rectangle(gradient_frame, (box_x2, box_y), (box_x2 + box_width, box_y + box_height),(255,255,255), -1)
+text2 = "Keybinds"
+text_size2 = cv2.getTextSize(text2, cv2.FONT_HERSHEY_TRIPLEX, 2, 3)[0]
+text_x2 = box_x2 + (box_width - text_size2[0]) // 2
+text_y2 = box_y + (box_height + text_size2[1]) // 2
+cv2.putText(gradient_frame, text2, (text_x2, text_y2), cv2.FONT_HERSHEY_TRIPLEX, 2, (0, 0, 0), 2)
+
+# Create instructions overlay
+instructions = gradient_frame.copy()
+inst_width, inst_height = int(W - W / 3), int(H - H/8)
+inst_x = (W - inst_width) // 2
+inst_y = (H - inst_height) // 2
+cv2.rectangle(instructions, (inst_x, inst_y), (inst_x + inst_width, inst_y + inst_height), (255, 255, 255), -1)
+
+lines_of_text = [
+    "Main ASL Mode Key Binds",
+    "Press ';' to enter Testing Mode"  ,
+    "Press '.' to enter Capture Mode",
+    "Press '/' to return to the main screen",
+    "Press '[' to change light/dark mode",
+    "",
+    "Capture Mode Key Binds",
+    "Type the word you would like to capture for",
+    "Press backspace to delete letters",
+    "Press enter to begin capturing",
+    "Press ';' to retrain AI with new adjustments",
+    "Press '/' to return to ASL mode",
+    "",
+    "Testing Mode Key Binds",
+    "Type the word you would like to capture for",
+    "Press backspace to delete letters",
+    "Press enter to capture a small adjustment",
+    "Press ']' to capture a large adjustment",
+    "Press ';' to retrain AI with new adjustments",
+    "Press '[' to change light/dark mode"
+    "",
+    "Press '/' to return to main screen",
+    "Press '/' in the main screen to quit"
+]
+
+inst_font = cv2.FONT_HERSHEY_SIMPLEX
+inst_font_scale = 1
+inst_font_thickness = 2
+inst_line_height = 40
+initial_y = inst_y + 30
+
+for i, text in enumerate(lines_of_text):
+    inst_text_size = cv2.getTextSize(text, inst_font, inst_font_scale, inst_font_thickness)[0]
+    inst_text_x = inst_x + (inst_width - inst_text_size[0]) // 2
+    inst_text_y = initial_y + i * inst_line_height
+    cv2.putText(instructions, text, (inst_text_x, inst_text_y), inst_font, inst_font_scale, (0, 0, 0), inst_font_thickness)
+
+box1_hover = False
+box2_hover = False
+instruction_show = False
+
+while True:
+    frame_copy = gradient_frame.copy()
+    if instruction_show:
+        cv2.imshow('frame', instructions)
+        if cv2.waitKey(25) == ord('/'):
+            instruction_show = False
+    else: 
+        if box1_hover:
+            cv2.rectangle(frame_copy, (box_x1, box_y), (box_x1 + box_width, box_y + box_height),(255,215,0), -1)
+        else:
+            cv2.rectangle(frame_copy, (box_x1, box_y), (box_x1 + box_width, box_y + box_height),(255,255,255), -1)
+        if box2_hover:
+            cv2.rectangle(frame_copy, (box_x2, box_y), (box_x2 + box_width, box_y + box_height),(255,215,0), -1)
+        else:
+            cv2.rectangle(frame_copy, (box_x2, box_y), (box_x2 + box_width, box_y + box_height),(255,255,255), -1)
+        cv2.putText(frame_copy, text1, (text_x1, text_y1), cv2.FONT_HERSHEY_TRIPLEX, 2, (0, 0, 0), 2)
+        cv2.putText(frame_copy, text2, (text_x2, text_y2), cv2.FONT_HERSHEY_TRIPLEX, 2, (0, 0, 0), 2)
+
+        cv2.imshow('frame', frame_copy)
+        if cv2.waitKey(25) == ord('/'):
+            break
+    cv2.setMouseCallback('frame', mouse_callback)
+
+cv2.destroyAllWindows()
---
+++ Meerkat_Main_F.py
@@ -0,0 +1,280 @@
+import os
+
+import pickle
+import subprocess
+import shutil
+
+import cv2
+import mediapipe as mp
+import numpy as np
+
+# Ensures there is a directory to store all image data
+RAW_IMG_DATA = './RAW_IMGS'
+if not os.path.exists(RAW_IMG_DATA):
+    os.makedirs(RAW_IMG_DATA)
+
+# load model
+model_dict = pickle.load(open('./model.pickle', 'rb'))
+model = model_dict['model']
+
+# set data sizes
+captureset_size = 250
+small_capture_size = 15
+large_capture_size = 50
+
+mode = 0
+
+cap = cv2.VideoCapture(1)
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+def capture_mode():
+    global model
+    current_symbol =''
+
+    while True:
+        to_exit = False
+
+        # Collects which word that data will be for
+        while True:
+            ret, frame = cap.read()
+            H, W, _ = frame.shape
+            cv2.putText(frame, 'Current Word: {}'.format(current_symbol), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 255), 3, cv2.LINE_AA)
+            cv2.imshow('frame', frame)
+            current_key = cv2.waitKey(25)
+            if current_key == ord('/'):
+                to_exit = True
+                break
+            elif current_key == 127:
+                current_symbol = current_symbol[:-1]
+            elif current_key == 13 and not current_symbol == '':
+                current_symbol = current_symbol.strip()
+                current_symbol = current_symbol.replace(" ", "_")
+                break
+            elif current_key == ord(';'):
+                ret, frame = cap.read()
+                black_image = np.zeros((H, W, 3), dtype=np.uint8)
+                cv2.putText(black_image, 'Proccessing, please wait', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 255), 3, cv2.LINE_AA)
+                cv2.imshow('frame', black_image)
+                cv2.waitKey(1)
+                #cv2.imshow('frame', frame)
+                subprocess.call(['python', 'Meerkat_Process'])
+                subprocess.call(['python', 'Meerkat_Train'])
+                model_dict = pickle.load(open('./model.pickle', 'rb'))
+                model = model_dict['model']
+            elif not current_key == -1:
+                current_symbol += (chr(current_key))
+                
+        # exits the program if the user wants to
+        if to_exit:
+            break
+        counter = 0
+        while counter < captureset_size:
+            ret, frame = cap.read()
+            cv2.imshow('frame', frame)
+            cv2.waitKey(25)
+            if not os.path.exists(os.path.join(RAW_IMG_DATA, current_symbol)):
+                os.makedirs(os.path.join(RAW_IMG_DATA, current_symbol))
+            cv2.imwrite(os.path.join(RAW_IMG_DATA, current_symbol, '{}.jpg'.format(counter)), frame)
+            counter += 1
+            if current_key == ord('/'):
+                to_exit = True
+                break
+        while True:
+            ret, frame = cap.read()
+            cv2.putText(frame, 'Save Data (Y/N)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 255), 3, cv2.LINE_AA)
+            cv2.imshow('frame', frame)
+            current_key = cv2.waitKey(25)
+            
+            if current_key == ord('Y') or current_key == ord('y'):
+                current_symbol = ''
+                break
+            elif current_key == ord('N') or current_key == ord('n'):
+                shutil.rmtree(os.path.join(RAW_IMG_DATA, current_symbol))
+                current_symbol = ''
+                break
+
+def testing_mode():
+    global mode
+    current_symbol = ''
+    model_dict = pickle.load(open('./model.pickle', 'rb'))
+    model = model_dict['model']
+    while True:
+
+        data_aux = []
+        x_ = []
+        y_ = []
+
+        ret, frame = cap.read()
+
+        H, W, _ = frame.shape
+
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        results = hands.process(frame_rgb)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    frame,  # image to draw
+                    hand_landmarks,  # model output
+                    mp_hands.HAND_CONNECTIONS,  # hand connections
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style())
+
+            for hand_landmarks in results.multi_hand_landmarks:
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+
+                    x_.append(x)
+                    y_.append(y)
+
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    data_aux.append(x)
+                    data_aux.append(y)
+            if len(data_aux) != 42:
+                continue
+            x1 = int(min(x_) * W) - 10
+            y1 = int(min(y_) * H) - 10
+
+            x2 = int(max(x_) * W) - 10
+            y2 = int(max(y_) * H) - 10
+
+            prediction = str(model.predict([np.asarray(data_aux)]))
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (mode, mode, mode), 4)
+            cv2.putText(frame, prediction, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (mode, mode, mode), 3,
+                        cv2.LINE_AA)
+
+        cv2.putText(frame, 'TESTING, TO CORRECT FOR: {}'.format(current_symbol), (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 255), 3, cv2.LINE_AA)
+        cv2.imshow('frame', frame)
+        pressed_key = cv2.waitKey(25)
+        if pressed_key == ord('/'):
+            break
+        elif pressed_key == 127:
+            current_symbol = current_symbol[:-1]
+        elif pressed_key == 13:
+            count = 0
+            if not os.path.exists(os.path.join(RAW_IMG_DATA, current_symbol)):
+                os.makedirs(os.path.join(RAW_IMG_DATA, current_symbol))
+            filename = len(os.listdir(os.path.join(RAW_IMG_DATA, current_symbol)))
+            while count <= small_capture_size:
+                ret, frame = cap.read()
+                cv2.imshow('frame', frame)
+                cv2.waitKey(25)
+                if not os.path.exists(os.path.join(RAW_IMG_DATA, current_symbol)):
+                    os.makedirs(os.path.join(RAW_IMG_DATA, current_symbol))
+                cv2.imwrite(os.path.join(RAW_IMG_DATA, current_symbol, '{}.jpg'.format(filename)), frame)
+                count += 1
+                filename += 1
+            current_symbol = ''
+        elif pressed_key == ord(']'):
+            count = 0
+            if not os.path.exists(os.path.join(RAW_IMG_DATA, current_symbol)):
+                os.makedirs(os.path.join(RAW_IMG_DATA, current_symbol))
+            filename = len(os.listdir(os.path.join(RAW_IMG_DATA, current_symbol)))
+            while count <= large_capture_size:
+                ret, frame = cap.read()
+                cv2.imshow('frame', frame)
+                cv2.waitKey(25)
+                if not os.path.exists(os.path.join(RAW_IMG_DATA, current_symbol)):
+                    os.makedirs(os.path.join(RAW_IMG_DATA, current_symbol))
+                cv2.imwrite(os.path.join(RAW_IMG_DATA, current_symbol, '{}.jpg'.format(filename)), frame)
+                count += 1
+                filename += 1
+        elif pressed_key == ord(';'):
+            ret, frame = cap.read()
+            black_image = np.zeros((H, W, 3), dtype=np.uint8)
+            cv2.putText(black_image, 'Proccessing, please wait', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 255), 3, cv2.LINE_AA)
+            cv2.imshow('frame', black_image)
+            cv2.waitKey(1)
+            #cv2.imshow('frame', frame)
+            subprocess.call(['python', 'Meerkat_Process'])
+            subprocess.call(['python', 'Meerkat_Train'])
+            model_dict = pickle.load(open('./model.pickle', 'rb'))
+            model = model_dict['model']
+        elif pressed_key == ord('['):
+            if mode == 0:
+                mode = 255
+            else:
+                mode = 0
+        elif not pressed_key == -1:
+                current_symbol += (chr(pressed_key))
+        
+def ASL_mode():
+    while True:
+        global model
+        global mode
+        data_aux = []
+        x_ = []
+        y_ = []
+
+        ret, frame = cap.read()
+
+        H, W, _ = frame.shape
+
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        results = hands.process(frame_rgb)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    frame,  # image to draw
+                    hand_landmarks,  # model output
+                    mp_hands.HAND_CONNECTIONS,  # hand connections
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style())
+
+            for hand_landmarks in results.multi_hand_landmarks:
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+
+                    x_.append(x)
+                    y_.append(y)
+
+                for i in range(len(hand_landmarks.landmark)):
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    data_aux.append(x)
+                    data_aux.append(y)
+            if len(data_aux) != 42:
+                continue
+            x1 = int(min(x_) * W) - 10
+            y1 = int(min(y_) * H) - 10
+
+            x2 = int(max(x_) * W) - 10
+            y2 = int(max(y_) * H) - 10
+
+            prediction = str(model.predict([np.asarray(data_aux)]))
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (mode, mode, mode), 4)
+            cv2.putText(frame, prediction, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (mode, mode, mode), 3,
+                        cv2.LINE_AA)
+
+        cv2.imshow('frame', frame)
+        pressed_key = cv2.waitKey(25)
+        if pressed_key == ord('/'):
+            break
+        elif pressed_key == ord(';'):
+            testing_mode()
+            model_dict = pickle.load(open('./model.pickle', 'rb'))
+            model = model_dict['model']
+        elif pressed_key == ord('.'):
+            capture_mode()
+        elif pressed_key == ord('['):
+            if mode == 0:
+                mode = 255
+            else:
+                mode = 0
+
+    
+ASL_mode()
+
+cap.release()
+cv2.destroyAllWindows()
